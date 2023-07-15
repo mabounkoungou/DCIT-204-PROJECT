024d8e83a67ae32d26846c2e08517aaaa7a547b1
Ghostwriter <mabounkoungou001@st.ug.edu.gh>
2023-07-15 23:29:13 +0000

    Delete group/src/com/group46/app directory

--- group/src/com/group46/app/ui/Main.java
+++
@@ -1,8 +0,0 @@
-package com.group46.app.ui;
-
-public class Main {
-    public static void main(String[] args) {
-        UserInterface userInterface = new UserInterface();
-        userInterface.displayDialogBox();
-    }
-}
